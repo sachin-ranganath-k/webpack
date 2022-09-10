ea5653a3a903e68507c744c27861f1049752c26c
ranganathnk <ranganathnkulkarni@gmail.com>
2022-09-09 17:02:47 -0700

    optimized folder structure

---
+++ INTRO/webpack.config.js
@@ -0,0 +1,10 @@
+const path=require('path');
+
+module.exports={
+ entry:"./src/index.js",  //Starting entry file
+ output:{
+    filename:"bundle.js",   //File named bundle.js will be outputted by webpack
+    path:path.resolve(__dirname,"dist") //bundle.js file will be created in the directory dist. 
+    //dist directory will be created by webpack later
+}
+}
